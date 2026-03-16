281ec7432dd2ca729154844a727f025db9890d2c
2024tm93590 <2024tm93590@wilp.bits-pilani.ac.in>
2026-03-16 20:46:31 +0530

    Project ready with tests

--- test_app.py
+++ test_app.py
@@ -1,11 +1,14 @@
 from app import ACEestApp
 import tkinter as tk
 
-def test_programs_exist():
+def test_program_templates_exist():
     root = tk.Tk()
     root.withdraw()   # prevents window opening
+
     app = ACEestApp(root)
 
-    assert "Fat Loss (FL)" in app.programs
-    assert "Muscle Gain (MG)" in app.programs
-    assert "Beginner (BG)" in app.programs
+    assert "Fat Loss" in app.program_templates
+    assert "Muscle Gain" in app.program_templates
+    assert "Beginner" in app.program_templates
+
+    root.destroy()
